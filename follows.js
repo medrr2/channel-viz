0a7369cff71d9182fd9778e240611b502a19a8c9
medrr2 <medrr2@aim.com>
2016-01-05 20:05:52 -0500

    Update follows.js

--- follows.js
+++ follows.js
@@ -11,11 +11,11 @@
 		hideForm		= 0;
 	*/
 
-	var defaultKey		= 'hTfurc6lunnOLeO8OvMv5iqyAUtmHHnGWrxjt1kenhBQ1xZp', // Unique master Xively API key to be used as a default
-		defaultFeeds	= [1061662570], // Comma separated array of Xively Feed ID numbers
+	var defaultKey		= '', // Unique master Xively API key to be used as a default
+		defaultFeeds	= [], // Comma separated array of Xively Feed ID numbers
 		applicationName	= '', // Replaces Xively logo in the header
-		dataDuration	= '0', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
-		dataInterval	= 60, // Default interval for data to be displayed (in seconds)
+		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
+		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
 		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
 		hideForm		= 0; // To hide input form use value of 1, otherwise set to 0
 
