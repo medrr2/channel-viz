ca74bd9cdbfc0571eb6e0bf09a3c5d5265379067
medrr2 <medrr2@aim.com>
2016-01-05 19:54:36 -0500

    Update follows.js

--- follows.js
+++ follows.js
@@ -1,24 +1,23 @@
 (function ( $ ){
 
-/*	
+	/*
 	EXAMPLE CONFIGURATION
-
-		var defaultKey	= '', // Unique master Xively API key to be used as a default
+		var defaultKey	= 'fje329iun52ngtuijo2f4jeun432A', // Unique master Xively API key to be used as a default
 		defaultFeeds	= [61916,12425,94322], // Comma separated array of Xively Feed ID numbers
 		applicationName	= 'My Company\'s Application', // Replaces Xively logo in the header
 		dataDuration	= '90days', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 10800, // Default interval for data to be displayed (in seconds)
 		dataColor		= '0A1922', // CSS HEX value of color to represent data (omit leading #)
 		hideForm		= 0;
-	
-*/
-		var defaultKey	= 'hTfurc6lunnOLeO8OvMv5iqyAUtmHHnGWrxjt1kenhBQ1xZp', // Unique master Xively API key to be used as a default
-		defaultFeeds	= [1061662570], // Comma separated array of Xively Feed ID numbers
-		applicationName	= 'OpenHab CPU Monitor', // Replaces Xively logo in the header
-		dataDuration	= '30days', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
-		dataInterval	= 10800, // Default interval for data to be displayed (in seconds)
-		dataColor	= 'FE9A2E', // CSS HEX value of color to represent data (omit leading #)
-		hideForm	= 1; // To hide input form use value of 1, otherwise set to 0
+	*/
+
+	var defaultKey		= '', // Unique master Xively API key to be used as a default
+		defaultFeeds	= [], // Comma separated array of Xively Feed ID numbers
+		applicationName	= '', // Replaces Xively logo in the header
+		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
+		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
+		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
+		hideForm		= 0; // To hide input form use value of 1, otherwise set to 0
 
 // Function Declarations
 
@@ -261,7 +260,7 @@
 						$('#feed-' + data.id + ' .tags').addClass('hidden');
 					}
 
-				/*	// Location
+					// Location
 					if(data.location) {
 						if(data.location.name || data.location.lat || data.location.ele || data.location.disposition) {
 
@@ -320,7 +319,7 @@
 							$('#feed-' + data.id + ' .elevation').addClass('hidden');
 							$('#feed-' + data.id + ' .disposition').addClass('hidden');
 							$('#feed-' + data.id + ' .map').addClass('hidden');
-					} */
+					}
 
 					$('#feed-' + data.id + ' .duration-hour').click(function() {
 						$('#loadingData').foundation('reveal', 'open');
