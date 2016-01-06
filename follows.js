5c4495d8312d7b1356ccb55d01b9345b18545a85
medrr2 <medrr2@aim.com>
2016-01-05 20:07:24 -0500

    Update follows.js

--- follows.js
+++ follows.js
@@ -11,12 +11,12 @@
 		hideForm		= 0;
 	*/
 
-	var defaultKey		= '', // Unique master Xively API key to be used as a default
-		defaultFeeds	= [], // Comma separated array of Xively Feed ID numbers
-		applicationName	= '', // Replaces Xively logo in the header
-		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
-		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
-		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
+	var defaultKey		= 'hTfurc6lunnOLeO8OvMv5iqyAUtmHHnGWrxjt1kenhBQ1xZp', // Unique master Xively API key to be used as a default
+		defaultFeeds	= [1061662570], // Comma separated array of Xively Feed ID numbers
+		applicationName	= 'OpenHab CPU Monitor', // Replaces Xively logo in the header
+		dataDuration	= '30days', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
+		dataInterval	= 10800, // Default interval for data to be displayed (in seconds)
+		dataColor		= '0A1922', // CSS HEX value of color to represent data (omit leading #)
 		hideForm		= 0; // To hide input form use value of 1, otherwise set to 0
 
 // Function Declarations
