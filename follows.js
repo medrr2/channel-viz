ad6f1b82b6a25eb56a5d6e0ee9eb24b1be930837
medrr2 <medrr2@aim.com>
2016-01-05 19:59:46 -0500

    Update follows.js

--- follows.js
+++ follows.js
@@ -16,8 +16,8 @@
 		applicationName	= 'OpenHab CPU Monitor', // Replaces Xively logo in the header
 		dataDuration	= '30days', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 10800, // Default interval for data to be displayed (in seconds)
-		dataColor		= 'FE9A2E', // CSS HEX value of color to represent data (omit leading #)
-		hideForm		= 1; // To hide input form use value of 1, otherwise set to 0
+		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
+		hideForm		= 0; // To hide input form use value of 1, otherwise set to 0
 
 // Function Declarations
 
